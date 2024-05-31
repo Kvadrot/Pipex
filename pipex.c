76774c37798a9a532f25dd4970ffdec5610a2e02
Kvadrot <send.me.your.maild@gmail.com>
2024-05-31 15:33:09 +0200

    temp works with 1 pipe, TODO: input reading

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 18:16:23 by itykhono          #+#    #+#             */
-/*   Updated: 2024/05/29 19:42:03 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/05/31 15:32:07 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,116 +19,9 @@ int ft_get_outfile_fd(char *filename) {
 }
 
 
-// char    *ft_exe_cmd(char **argvs, int infile_fd) {
-//     char **exe_arguments;
-//     int pid_forChild;
-//     int i;
-//     char *result;
-//     int pipe_fd[2];
-//     char content[10];
-
-//     result = NULL;
-//     exe_arguments = NULL;
-
-//     // Create MainPipe
-//     if (pipe(pipe_fd) == -1) {
-//         ft_printf("%s", strerror(errno));
-//         return NULL;
-//     }
-
-// 	//TODO: Read till the end of file
-//     if ((read(infile_fd, content, 10)) < 0) {
-//         ft_printf("wrong read\n");
-//         return (NULL);
-//     }
-
-//     // Rewrite from the infile into MainPipe.write
-//     write(pipe_fd[1], content, 10);
-//     close(infile_fd);
-
-//     // Create Child Process
-//     pid_forChild = fork();
-//     if (pid_forChild < 0) {
-//         ft_printf("%s", strerror(errno));
-//         return (NULL);
-//     }
-
-
-
-//     if (pid_forChild == 0) {
-//         // Child process
-//         i = 2;
-//         ft_printf("child Process is running\n");
-		
-//         while (argvs[i] != NULL) {
-//             // Create GrandChild Process
-
-//             int grandChild = fork();
-//             if (grandChild < 0) {
-//                 ft_printf("%s", strerror(errno));
-//                 return (NULL);
-//             }
-
-// 			if (grandChild == 0) {
-
-// 			if (dup2(pipe_fd[0], STDIN_FILENO) < 0) {
-// 				ft_printf("Here is %s", strerror(errno));
-// 				close(pipe_fd[0]);
-// 				close(pipe_fd[1]);
-// 				exit(-111);
-// 			}
-// 			close(pipe_fd[0]); // Close read end in child after loop
-
-// 			int execProcess = fork();
-// 			if (execProcess < 0) {
-//                 ft_printf("%s", strerror(errno));
-//                 return (NULL);
-//             }
-// 			if (execProcess == 0)
-// 			{
-// 				if (dup2(pipe_fd[1], STDOUT_FILENO) < 0) {
-// 				ft_printf("%s", strerror(errno));
-// 				close(pipe_fd[0]);
-// 				close(pipe_fd[1]);
-// 				exit(-111);
-// 				}
-// 				close(pipe_fd[1]);
-// 				exe_arguments = ft_split(argvs[i], ' ');
-// 				if (!exe_arguments) {
-//           	      exit(-333);
-// 				}
-// 				char *path_cmd = ft_strjoin("/bin/", exe_arguments[0]);
-//           	  // Execute CMD
-// 			  execve(path_cmd, exe_arguments, NULL);
-// 			}
-// 			wait(NULL);
-// 			close(pipe_fd[1]); // Close write end in grandchild after dup
-//             // Prepare CMD arguments
-
-
-//             } else {
-//                 // ChildProcess is waiting for Grandchild
-//                 wait(NULL);
-//                 i++;
-//             }
-// 			exit(0);
-//         }
-//     } else {
-// 		close(pipe_fd[0]);
-// 		close(pipe_fd[1]);
-//         wait(NULL); // Wait for child to finish
-
-//         // Read the result from the pipe
-//         // read(pipe_fd[0], content, 10);
-//         // close(pipe_fd[0]);
-//         // write(1, content, 10);
-//     }
-
-//     return (result);
-// }
 char *read_input(int inputfd) {
 
-	return ("wc");
+	return ("hello Wprld");
 }
 
 static void ft_close_all_pipes(int all_pipes[][2], int pipes_amount)
@@ -137,120 +30,166 @@ static void ft_close_all_pipes(int all_pipes[][2], int pipes_amount)
 	i = 0;
 	while (i < pipes_amount)
 	{
-		close(all_pipes[i][0]);
-		close(all_pipes[i][1]);
+		if(all_pipes[i][0])
+		{
+			close(all_pipes[i][0]);
+		}
+		if(all_pipes[i][1])
+		{
+			close(all_pipes[i][1]);
+		}
 		i++;
 	}
 }
 
-int	ft_create_pipes(int pipe_amount, int created_pipes[][2])
-{
-	int i;
-	i = 0;
-	while (i < pipe_amount)
-	{
-		pipe(created_pipes[i]);
-		if (pipe(created_pipes[i]) == -1) {
-			while (i >= 0 && created_pipes[i] != NULL)
-			{
-				close(created_pipes[i][1]);
-				close(created_pipes[i][0]);
-				i--;
-			}
-        	ft_printf("%s", strerror(errno));
-        	return -242;
-    	}
-		i++;	
-	}
-	printf("pipe_created_counter: %d\n",i);
-	return (200);
+int ft_create_pipes(int pipe_amount, int created_pipes[][2]) {
+    int i;
+    i = 0;
+    while (i < pipe_amount) {
+        if (pipe(created_pipes[i]) == -1) {
+            while (i >= 0) {
+                close(created_pipes[i][1]);
+                close(created_pipes[i][0]);
+                i--;
+            }
+            ft_printf("%s", strerror(errno));
+            return -242;
+        }
+        i++;
+    }
+    ft_printf("pipe_created_counter: %d\n", i);
+    return (200);
 }
-char    *ft_exe_cmd(char **argvs, int infile_fd, int cmds) {
-	char	**exe_arguments;
-	int		pipes_fd[cmds][2];
-	int		child_fd;
-	int		grandchild;
 
-	//Create all Pipes
-	if (ft_create_pipes(cmds, pipes_fd) == -242)
-	{
-		ft_printf("%s", strerror(errno));
-		return (NULL);
-	}
-	write(pipes_fd[0][1],read_input(infile_fd), ft_strlen(read_input(infile_fd)));
-	close(infile_fd);
-	
-	if ((child_fd = fork()) < 0)
-	{
-		ft_close_all_pipes(pipes_fd, cmds);
-		ft_printf("%s", strerror(errno));
-	}
-	close(pipes_fd[0][1]);
-	close(pipes_fd[0][0]);
+char *ft_exe_cmd(char **argvs, int infile_fd, int cmds) {
+    char **exe_arguments;
+    int pipes_fd[cmds + 1][2];
+    int child_fd;
+    int grandchild;
+    char *result_str = (char *)malloc(100);
 
-	//ONLY CHILD ACCESS
-	if (child_fd == 0)
-	{
-		// Create GrandChild Process
-		int grandChild = fork();
-		if (grandChild < 0) {
-			ft_close_all_pipes(pipes_fd, cmds);
-			ft_printf("%s", strerror(errno));
-			return (NULL);
-		}
-		close(pipes_fd[0][1]);
-		close(pipes_fd[0][0]);
-		if (grandChild == 0)
-		{
-			//redirect input for execve
-			if (dup2(pipes_fd[0][0], STDIN_FILENO) < 0);
-			{
-				ft_close_all_pipes(pipes_fd, cmds);
-				ft_printf("%s", strerror(errno));
-				// close(pipes_fd[0][1]);
-				// close(pipes_fd[0][0]);
-				return (NULL);
-			}
-			close(pipes_fd[0][0]);
-			close(pipes_fd[0][1]);
+    // Create all Pipes
+    if (ft_create_pipes(cmds + 1, pipes_fd) == -242) {
+        ft_printf("%s", strerror(errno));
+        return (NULL);
+    }
 
-			//redirect output for execve
-			if ( dup2(pipes_fd[1][1], STDIN_FILENO) < 0);
-			{
-				ft_close_all_pipes(pipes_fd, cmds);
-				ft_printf("%s", strerror(errno));
-				// close(pipes_fd[1][1]);
-				// close(pipes_fd[1][0]);
-				return (NULL);
-			}
-			close(pipes_fd[1][0]);
-			close(pipes_fd[1][1]);
+    write(pipes_fd[0][1], read_input(infile_fd), 10);
+    close(pipes_fd[0][1]); // Close the write end of the first pipe
+    close(infile_fd);
 
-			exe_arguments = ft_split(argvs[2], ' ');
-			if (!exe_arguments) {
-				ft_close_all_pipes(pipes_fd, cmds);
-				ft_printf("%s", strerror(errno));
-				exit(-333);
-			}
-			char *path_cmd = ft_strjoin("/bin/", exe_arguments[0]);
-			if (!path_cmd)
-			{
-				ft_close_all_pipes(pipes_fd, cmds);
-				ft_printf("%s", strerror(errno));
-				exit(-333);
-			}
-			// Execute CMD
-			execve(path_cmd, exe_arguments, NULL);
-		} else {
-			//Child Process is waiting for its children
-			wait(NULL);
-			read(pipes_fd[0][0], );
-		}
-	} else {
-		//MAIN Process is waiting for its children
-		wait(NULL);
-	}
+    //DEBUG PRINT
+    // char temp[10];
+    // read(pipes_fd[0][0], temp, 10);
+    // ft_printf("String was written to the pipes_fd[0][0]: %s\n", temp);
+    // ft_printf("main closed: pipes_fd[0][1]\n");
+    // //DEBUG PRINT
+
+    if ((child_fd = fork()) < 0) {
+        ft_close_all_pipes(pipes_fd, cmds + 1);
+        ft_printf("%s", strerror(errno));
+        return (NULL);
+    }
+
+    // ONLY CHILD ACCESS
+    if (child_fd == 0) {
+        if (dup2(pipes_fd[0][0], STDIN_FILENO) < 0) {
+            ft_close_all_pipes(pipes_fd, cmds + 1);
+            ft_printf("dup2 failed for stdin in child: %s\n", strerror(errno));
+            return (NULL);
+        }
+        close(pipes_fd[0][0]);
+
+        if (dup2(pipes_fd[1][1], STDOUT_FILENO) < 0) {
+            ft_close_all_pipes(pipes_fd, cmds + 1);
+            ft_printf("dup2 failed for stdout in child: %s\n", strerror(errno));
+            return (NULL);
+        }
+        close(pipes_fd[1][1]);
+
+        // Close all other pipes
+        close(pipes_fd[1][0]);
+        close(pipes_fd[2][1]);
+        close(pipes_fd[2][0]);
+
+        // Execute CMD
+        exe_arguments = ft_split(argvs[2], ' ');
+        if (!exe_arguments) {
+            ft_close_all_pipes(pipes_fd, cmds + 1);
+            exit(-333);
+        }
+        char *path_cmd = ft_strjoin("/bin/", exe_arguments[0]);
+        if (!path_cmd) {
+            ft_close_all_pipes(pipes_fd, cmds + 1);
+            exit(-333);
+        }
+        execve(path_cmd, exe_arguments, NULL);
+        ft_printf("execve failed for child: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    if ((grandchild = fork()) < 0) {
+        ft_close_all_pipes(pipes_fd, cmds + 1);
+        ft_printf("%s", strerror(errno));
+        return (NULL);
+    }
+
+    if (grandchild == 0) {
+        close(pipes_fd[0][0]);
+
+        if (dup2(pipes_fd[1][0], STDIN_FILENO) < 0) {
+            ft_close_all_pipes(pipes_fd, cmds + 1);
+            ft_printf("dup2 failed for stdin in grandchild: %s\n", strerror(errno));
+            exit(-434);
+        }
+        close(pipes_fd[1][0]);
+
+        if (dup2(pipes_fd[2][1], STDOUT_FILENO) < 0) {
+            ft_close_all_pipes(pipes_fd, cmds + 1);
+            ft_printf("dup2 failed for stdout in grandchild: %s\n", strerror(errno));
+            exit(-435);
+        }
+        close(pipes_fd[2][1]);
+
+        // Close all other pipes
+        close(pipes_fd[1][1]);
+        close(pipes_fd[2][0]);
+
+        // Execute CMD
+        exe_arguments = ft_split(argvs[3], ' ');
+        if (!exe_arguments) {
+            ft_close_all_pipes(pipes_fd, cmds + 1);
+            exit(-333);
+        }
+        char *path_cmd = ft_strjoin("/bin/", exe_arguments[0]);
+        if (!path_cmd) {
+            ft_close_all_pipes(pipes_fd, cmds + 1);
+            exit(-333);
+        }
+        execve(path_cmd, exe_arguments, NULL);
+        ft_printf("execve failed for grandchild: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    // Close all pipes in the parent process
+    close(pipes_fd[0][0]);
+    close(pipes_fd[1][0]);
+    close(pipes_fd[1][1]);
+    close(pipes_fd[2][1]);
+
+    // Wait for children to complete
+    wait(NULL);
+
+    //DEBUG PRINT
+    char temp2[10];
+    read(pipes_fd[2][0], temp2, 10);
+    ft_printf("pipes_fd[2][0] Contains: %s\n", temp2);
+    //DEBUG PRINT
+
+    close(pipes_fd[2][0]);
+    return result_str;
 }
+
 
 
 int main(int argc, char **argv)
@@ -271,6 +210,7 @@ int main(int argc, char **argv)
 		return (1);		
 	}
 	// TODO: cmds coutnter + otput_fd_opener
-	ft_exe_cmd(argv, inputfd, argc - 3);
+	int cmds = argc - 3;
+	ft_exe_cmd(argv, inputfd, cmds);
 	close(inputfd);	
 }
