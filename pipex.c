f4d4e53b3da39773afe9c45d7fa905b06e1f31e8
Kvadrot <send.me.your.maild@gmail.com>
2024-06-02 18:22:21 +0200

    works with multi pipes, TODO: fix printf(write to the stdOUT) output writing

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 18:16:23 by itykhono          #+#    #+#             */
-/*   Updated: 2024/06/02 14:24:42 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/06/02 17:53:51 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,17 @@ char *read_input(int inputfd) {
 	return (result);
 }
 
-static void ft_close_all_pipes(int all_pipes[][2], int pipes_amount)
+static void ft_close_all_pipes(int all_pipes[][2], int pipes_amount, int *exeption_pipe)
 {
 	int	i;
 	i = 0;
 	while (i < pipes_amount)
 	{
-		if(all_pipes[i][0])
+		if(all_pipes[i][0] && all_pipes[i][0] != *exeption_pipe)
 		{
 			close(all_pipes[i][0]);
 		}
-		if(all_pipes[i][1])
+		if(all_pipes[i][1] && all_pipes[i][1] != *exeption_pipe)
 		{
 			close(all_pipes[i][1]);
 		}
@@ -61,7 +61,7 @@ int ft_create_pipes(int pipe_amount, int created_pipes[][2]) {
         }
         i++;
     }
-    // ft_printf("pipe_created_counter: %d\n", i);
+    ft_printf("pipe_created_counter: %d\n", i);
     return (200);
 }
 
@@ -85,13 +85,6 @@ char *ft_exe_cmd(char **argvs, int infile_fd, int cmds) {
     write(pipes_fd[0][1], input_str, ft_strlen(input_str));
     close(pipes_fd[0][1]); // Close the write end of the first pipe
 	free(input_str);
-	
-    // // DEBUG PRINT
-    // char temp[25];
-    // read(pipes_fd[0][0], temp, 25);
-    // ft_printf("String was written to the pipes_fd[0][0]: %s\n", temp);
-    // ft_printf("main closed: pipes_fd[0][1]\n");
-    // //DEBUG PRINT
 
 	pid = 2;
 	while (cmd_index < cmds)
@@ -102,7 +95,7 @@ char *ft_exe_cmd(char **argvs, int infile_fd, int cmds) {
 		}
 		if (pid < 0)
 		{
-			ft_close_all_pipes(pipes_fd, cmds + 1);
+			ft_close_all_pipes(pipes_fd, cmds + 1, NULL);
 			ft_printf("%s", strerror(errno));
 			return (NULL);	
 		}
@@ -110,54 +103,53 @@ char *ft_exe_cmd(char **argvs, int infile_fd, int cmds) {
 		if (pid == 0) {
 		// ONLY CHILD ACCESS
 			if (dup2(pipes_fd[cmd_index][0], STDIN_FILENO) < 0) {
-				ft_close_all_pipes(pipes_fd, cmds + 1);
+				ft_close_all_pipes(pipes_fd, cmds + 1, NULL);
 				ft_printf("dup2 failed for stdin in child: %s\n", strerror(errno));
 				return (NULL);
 			}
 			
 			if (dup2(pipes_fd[cmd_index + 1][1], STDOUT_FILENO) < 0) {
-				ft_close_all_pipes(pipes_fd, cmds + 1);
+				ft_close_all_pipes(pipes_fd, cmds + 1, NULL);
 				ft_printf("dup2 failed for stdout in child: %s\n", strerror(errno));
 				return (NULL);
 			}
 			close(pipes_fd[0][0]);
 			close(pipes_fd[1][1]);
 
-			// Close all other pipes
-			close(pipes_fd[1][0]);
-			close(pipes_fd[2][1]);
-			close(pipes_fd[2][0]);
-
+			// // Close all other pipes
+			// close(pipes_fd[1][0]);
+			// close(pipes_fd[2][1]);
+			// close(pipes_fd[2][0]);
+			// close(pipes_fd[3][1]);
+			// close(pipes_fd[3][0]);
+			ft_close_all_pipes(pipes_fd, cmds + 1, &(pipes_fd[0][1]));
 			// Execute CMD
 			exe_arguments = ft_split(argvs[cmd_index + 2], ' '); //prog name + infile 
 			if (!exe_arguments) {
-				ft_close_all_pipes(pipes_fd, cmds + 1);
+				// ft_close_all_pipes(pipes_fd, cmds + 1, NULL);
 				exit(-333);
 			}
 			char *path_cmd = ft_strjoin("/bin/", exe_arguments[0]);
 			if (!path_cmd) {
-				ft_close_all_pipes(pipes_fd, cmds + 1);
+				// ft_close_all_pipes(pipes_fd, cmds + 1, NULL);
 				exit(-333);
 			}
+			// ft_printf("cmd_path = %s\n", path_cmd);
 			execve(path_cmd, exe_arguments, NULL);
 		}
 		cmd_index++;
+		// ft_printf("cmd_index = %d\n", cmd_index);
 	}
     // Close all pipes in the parent process
-    close(pipes_fd[0][0]);
-    close(pipes_fd[1][0]);
-    close(pipes_fd[1][1]);
-    close(pipes_fd[2][1]);
-
+    // close(pipes_fd[0][0]);
+    // close(pipes_fd[1][0]);
+    // close(pipes_fd[1][1]);
+    // close(pipes_fd[2][1]);
+	ft_close_all_pipes(pipes_fd, cmds + 1, &(pipes_fd[cmds][0]));
     // Wait for children to complete
     wait(NULL);
 
-    //DEBUG PRINT
-    // char temp2[30];
-    // read(pipes_fd[2][0], temp2, 30);
-    // ft_printf("pipes_fd[2][0] Contains: %s\n", temp2);
-    //DEBUG PRINT
-	result_str = read_input(pipes_fd[2][0]);
+	result_str = read_input(pipes_fd[cmds][0]);
     close(pipes_fd[2][0]);
     return result_str;
 }
